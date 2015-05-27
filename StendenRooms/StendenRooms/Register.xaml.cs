aca663f803948522dea5d5a26d0d427eee456970
Arjan Weurding <arie_weur@live.nl>
2015-05-27 13:10:34 +0200

    Bug fixes

--- StendenRooms/StendenRooms/Register.xaml.cs
+++ StendenRooms/StendenRooms/Register.xaml.cs
@@ -20,9 +20,9 @@ namespace StendenRooms
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Overview : Page
+    public sealed partial class Register : Page
     {
-        public Overview()
+        public Register()
         {
             this.InitializeComponent();
         }
